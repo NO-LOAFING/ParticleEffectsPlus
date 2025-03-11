5edf0c9556dab80e23dd5cd47e7298a0b6efd2d9
NO-LOAFING <NO-LOAFING@users.noreply.github.com>
2025-03-11 15:48:17 -0700

    Added info text for some unintuitive particles

--- lua/autorun/partctrl_autorun.lua
+++ lua/autorun/partctrl_autorun.lua
@@ -56,6 +56,102 @@ hook.Add("PartCtrl_PostProcessPCF", "default_blacklist", function(filename, tab)
 end)
 
 
+//Default comments for unintuitive fx
+local stove_comment = "Control point 1 pushes the flame away; it's very subtle"
+local tooclose_comment = "Not visible if too close to the camera"
+local cullplane1_comment = "Control point 1 controls max height of particles"
+local cullplane1_bubble_comment = "Control point 1 controls max height of bubbles"
+local cullplane1_reverse_comment = "Control point 1 controls minimum height of particles"
+local default_comments = {
+	//Default
+	["particles/fire_01.pcf"] = {
+		burning_vehicle = tooclose_comment
+	},
+	["particles/water_impact.pcf"] = {
+		water_bubble_trail_1 = cullplane1_bubble_comment
+	},
+	//Team Fortress 2
+	["particles/halloween2024_unusuals.pcf"] = {
+		unusual_stove_flame_point_1 = stove_comment,
+		unusual_stove_flame_point_1_red = stove_comment,
+		unusual_stove_flame_point_2 = stove_comment,
+		unusual_stove_flame_point_2_red = stove_comment,
+		unusual_stove_flame_point_3 = stove_comment,
+		unusual_stove_flame_point_3_red = stove_comment,
+		unusual_stove_flame_point_4 = stove_comment,
+		unusual_stove_flame_point_4_red = stove_comment,
+		unusual_stove_flame_point_5 = stove_comment,
+		unusual_stove_flame_point_5_red = stove_comment,
+		unusual_stove_flame_point_6 = stove_comment,
+		unusual_stove_flame_point_6_red = stove_comment,
+		unusual_stove_flame_point_7 = stove_comment,
+		unusual_stove_flame_point_7_red = stove_comment,
+		unusual_stove_flame_point_8 = stove_comment,
+		unusual_stove_flame_point_8_red = stove_comment,
+	},
+	["particles/coin_spin.pcf"] = {
+		coin_spin = "Only creates particles while moving"
+	},
+	["particles/stamp_spin.pcf"] = {
+		Stamp_spin = "Only creates particles while moving"
+	},
+	["particles/taunt_fx.pcf"] = {
+		taunt_demo_nuke_brew = cullplane1_comment,
+		taunt_demo_nuke_powder = cullplane1_comment,
+	},
+	["particles/water.pcf"] = {
+		water_playerdive = cullplane1_bubble_comment,
+		water_playerdive_bubbles = cullplane1_bubble_comment,
+		water_splash_croc_spawn_droplets = cullplane1_reverse_comment,
+	},
+	["particles/rockettrail.pcf"] = {
+		rockettrail_underwater = cullplane1_bubble_comment,
+		rockettrail_waterbubbles = cullplane1_bubble_comment,
+	},
+	//Portal
+	["particles/neurotoxins.pcf"] = {
+		neurotoxins_step1 = tooclose_comment,
+	},
+	["particles/portals.pcf"] = {
+		Portal_1_vacuum = tooclose_comment,
+		portal_2_vacuum = tooclose_comment,
+		portal_1_particles = tooclose_comment,
+		portal_2_particles = tooclose_comment,
+	},
+	["particles/tubes.pcf"] = {
+		broken_tube_suck_b = tooclose_comment,
+	},
+	["particles/portal_projectile.pcf"] = {
+		portal_2_overlap_ = "Control point 1 pushes particles away"
+	},
+	//TF2 map particles addon
+	["particles/koth_probed_fx.pcf"] = {
+		alien_abduction_glow2 = tooclose_comment
+	},
+	//Invasion mashup pack addon
+	["particles/alien_fantasmos_fx2_extras.pcf"] = { 
+		alien_jumppad_centerglow3 = "Control point 0 pulls particles toward itself; it's very subtle"
+	},
+}
+function PartCtrl_AppendInfoText(effecttab, str) //global function so other addons using this hook can use it
+	if !effecttab.info then
+		effecttab.info = ""
+	else
+		effecttab.info = effecttab.info .. "\n"
+	end
+	effecttab.info = effecttab.info .. str
+end
+hook.Add("PartCtrl_PostProcessPCF", "default_comments", function(filename, tab)
+	if default_comments[filename] then
+		for k, v in pairs (tab) do
+			if default_comments[filename][k] then
+				PartCtrl_AppendInfoText(tab[k], default_comments[filename][k])
+			end
+		end
+	end
+end)
+
+
 
 
 
@@ -3691,6 +3787,10 @@ function PartCtrl_ProcessPCF(filename)
 				shouldcull = shouldcull .. "This particle effect has the value preventNameBasedLookup set to true, which prevents\nthe game from spawning it directly, though other effects can still use it as a child."
 			end
 			t2[particle].shouldcull = shouldcull
+			//Also add info text for viewmodel effects here, because this isn't inherited
+			if t[particle]["view model effect"] then
+				PartCtrl_AppendInfoText(t2[particle], "Viewmodel effect: draws in front of everything, and has a distorted position unless attached to a model on a non-0 attachment.")
+			end
 		end
 		//Now that the processed table is finished, let hook funcs modify it arbitrarily (including deciding which fx to cull)
 		hook.Call("PartCtrl_PostProcessPCF", nil, filename, t2)
