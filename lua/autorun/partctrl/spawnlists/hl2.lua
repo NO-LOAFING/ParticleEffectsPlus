b22870f02973f71251f533b7883413aaf0de53c0
NO-LOAFING <NO-LOAFING@users.noreply.github.com>
2025-12-23 10:09:03 -0800

    Reorder HL2 spawnlist a little

--- lua/autorun/partctrl/spawnlists/hl2.lua
+++ lua/autorun/partctrl/spawnlists/hl2.lua
@@ -8,230 +8,230 @@
 		"1"
 		{
 			"type"		"header"
-			"text"		"Dev/Test Effects"
+			"text"		"This effect makes nearby ropes move"
 		}
 		"2"
 		{
 			"type"		"partctrl"
-			"path"		"garrysmod"
-			"name"		"error"
-			"pcf"		"particles/error.pcf"
+			"name"		"ShakeRopes"
+			"pcf"		"UtilFx"
 		}
 		"3"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"Weapon_Combine_Ion_Cannon_Backup"
-			"pcf"		"particles/weapon_fx.pcf"
-		}
-		"4"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"default"
-			"pcf"		"particles/default.pcf"
-		}
-		"5"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"bonfire"
-			"pcf"		"particles/bonfire.pcf"
-		}
-		"6"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"bonfire"
-			"pcf"		"particles/fireflow.pcf"
-		}
-		"7"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"smoke_blackbillow"
-			"pcf"		"particles/largefire.pcf"
-		}
-		"8"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"flame"
-			"pcf"		"particles/flamethrowertest.pcf"
-		}
-		"9"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"lit_smoke_sphere"
-			"pcf"		"particles/demo_particle_light.pcf"
-		}
-		"10"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"lit_smoke_sphere_sub"
-			"pcf"		"particles/demo_particle_light.pcf"
-		}
-		"11"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"test_green_alpha_sequence"
-			"pcf"		"particles/test_grnalpha.pcf"
-		}
-		"12"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"smoke_oily_01"
-			"pcf"		"particles/fire_ring.pcf"
-		}
-		"13"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"vomit"
-			"pcf"		"particles/test_noise.pcf"
-		}
-		"14"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"test_beam"
-			"pcf"		"particles/devtest.pcf"
-		}
-		"15"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"test_collision"
-			"pcf"		"particles/devtest.pcf"
-		}
-		"16"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"test_orientation"
-			"pcf"		"particles/devtest.pcf"
-		}
-		"17"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"test_lighting"
-			"pcf"		"particles/devtest.pcf"
-		}
-		"18"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"test_trails"
-			"pcf"		"particles/devtest.pcf"
-		}
-		"19"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"demo_falling_sand"
-			"pcf"		"particles/sand.pcf"
-		}
-		"20"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"demo_falling_sand_sub"
-			"pcf"		"particles/sand.pcf"
-		}
-		"21"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"demo_aurora_01"
-			"pcf"		"particles/aurora_sphere2.pcf"
-		}
-		"22"
-		{
-			"type"		"partctrl"
-			"path"		"hl2"
-			"name"		"aurora_shockwave"
-			"pcf"		"particles/aurora_sphere2.pcf"
-		}
-		"23"
-		{
-			"type"		"header"
-			"text"		"These unused things that"
-		}
-		"24"
-		{
-			"type"		"header"
-			"text"		"could work as laser pointers"
-		}
-		"25"
-		{
-			"type"		"partctrl"
-			"name"		"CommandPointer"
-			"pcf"		"UtilFx"
-		}
-		"26"
-		{
-			"type"		"partctrl"
-			"name"		"GunshipImpact"
-			"pcf"		"UtilFx"
-		}
-		"27"
 		{
 			"type"		"header"
 			"text"		"This nice oil drip effect that's"
 		}
-		"28"
+		"4"
 		{
 			"type"		"header"
 			"text"		"pointed the wrong way"
 		}
-		"29"
+		"5"
 		{
 			"type"		"partctrl"
 			"path"		"garrysmod"
 			"name"		"blood_drip_synth_01b"
 			"pcf"		"particles/blood_impact.pcf"
 		}
-		"30"
+		"6"
 		{
 			"type"		"header"
-			"text"		"This effect makes nearby ropes move"
+			"text"		"These unused things that"
 		}
-		"31"
+		"7"
+		{
+			"type"		"header"
+			"text"		"could work as laser pointers"
+		}
+		"8"
 		{
 			"type"		"partctrl"
-			"name"		"ShakeRopes"
+			"name"		"CommandPointer"
 			"pcf"		"UtilFx"
 		}
-		"32"
+		"9"
+		{
+			"type"		"partctrl"
+			"name"		"GunshipImpact"
+			"pcf"		"UtilFx"
+		}
+		"10"
 		{
 			"type"		"header"
 			"text"		"Frozen effects from Ep2 Gman scene"
 		}
-		"33"
+		"11"
 		{
 			"type"		"partctrl"
 			"path"		"hl2"
 			"name"		"blood_frozen"
 			"pcf"		"particles/choreo_gman.pcf"
 		}
-		"34"
+		"12"
 		{
 			"type"		"partctrl"
 			"path"		"hl2"
 			"name"		"frozen_steam"
 			"pcf"		"particles/choreo_gman.pcf"
 		}
-		"35"
+		"13"
 		{
 			"type"		"partctrl"
 			"path"		"hl2"
 			"name"		"muzzleflash_frozen"
 			"pcf"		"particles/choreo_gman.pcf"
+		}
+		"14"
+		{
+			"type"		"header"
+			"text"		"Dev/Test Effects"
+		}
+		"15"
+		{
+			"type"		"partctrl"
+			"path"		"garrysmod"
+			"name"		"error"
+			"pcf"		"particles/error.pcf"
+		}
+		"16"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"Weapon_Combine_Ion_Cannon_Backup"
+			"pcf"		"particles/weapon_fx.pcf"
+		}
+		"17"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"default"
+			"pcf"		"particles/default.pcf"
+		}
+		"18"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"bonfire"
+			"pcf"		"particles/bonfire.pcf"
+		}
+		"19"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"bonfire"
+			"pcf"		"particles/fireflow.pcf"
+		}
+		"20"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"smoke_blackbillow"
+			"pcf"		"particles/largefire.pcf"
+		}
+		"21"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"flame"
+			"pcf"		"particles/flamethrowertest.pcf"
+		}
+		"22"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"lit_smoke_sphere"
+			"pcf"		"particles/demo_particle_light.pcf"
+		}
+		"23"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"lit_smoke_sphere_sub"
+			"pcf"		"particles/demo_particle_light.pcf"
+		}
+		"24"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"test_green_alpha_sequence"
+			"pcf"		"particles/test_grnalpha.pcf"
+		}
+		"25"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"smoke_oily_01"
+			"pcf"		"particles/fire_ring.pcf"
+		}
+		"26"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"vomit"
+			"pcf"		"particles/test_noise.pcf"
+		}
+		"27"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"test_beam"
+			"pcf"		"particles/devtest.pcf"
+		}
+		"28"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"test_collision"
+			"pcf"		"particles/devtest.pcf"
+		}
+		"29"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"test_orientation"
+			"pcf"		"particles/devtest.pcf"
+		}
+		"30"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"test_lighting"
+			"pcf"		"particles/devtest.pcf"
+		}
+		"31"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"test_trails"
+			"pcf"		"particles/devtest.pcf"
+		}
+		"32"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"demo_falling_sand"
+			"pcf"		"particles/sand.pcf"
+		}
+		"33"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"demo_falling_sand_sub"
+			"pcf"		"particles/sand.pcf"
+		}
+		"34"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"demo_aurora_01"
+			"pcf"		"particles/aurora_sphere2.pcf"
+		}
+		"35"
+		{
+			"type"		"partctrl"
+			"path"		"hl2"
+			"name"		"aurora_shockwave"
+			"pcf"		"particles/aurora_sphere2.pcf"
 		}
 	}
 	"name"		"Half-Life 2 & Episodes"
