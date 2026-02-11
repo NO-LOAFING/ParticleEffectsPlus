09821d8281dd6485bbfa4f89556c1a5b9a94006b
NO-LOAFING <NO-LOAFING@users.noreply.github.com>
2026-02-11 08:20:27 -0800

    Back up lists of bad fx to addon - Not used anywhere, just included so that the bad effect todo list is backed up by git

---
+++ lua/autorun/partctrl/spawnlists/bad_01.lua
@@ -0,0 +1,982 @@
+"TableToKeyValues"
+{
+	"parentid"		"60"
+	"icon"		"icon16/folder.png"
+	"id"		"61"
+	"contents"
+	{
+		"1"
+		{
+			"type"		"header"
+			"text"		"bad: fin sprites are rendering flat, unlike"
+		}
+		"2"
+		{
+			"type"		"header"
+			"text"		"in TF2. this is because operator ''Set child "
+		}
+		"3"
+		{
+			"type"		"header"
+			"text"		"control points from particle positions'' looks"
+		}
+		"4"
+		{
+			"type"		"header"
+			"text"		"for a different var name in gmod than in TF2 -"
+		}
+		"5"
+		{
+			"type"		"header"
+			"text"		"TF looks for ''Set cp orientation for particles'',"
+		}
+		"6"
+		{
+			"type"		"header"
+			"text"		"while gmod looks for ''set orientation''."
+		}
+		"7"
+		{
+			"type"		"partctrl"
+			"path"		"tf"
+			"name"		"utaunt_sharkfin2_parent"
+			"pcf"		"particles/summer2025_unusuals.pcf"
+		}
+		"8"
+		{
+			"type"		"partctrl"
+			"path"		"garrysmod"
+			"name"		"utaunt_sharkfin2_fin1_upright_setter"
+			"pcf"		"particles/summer2025_unusuals.pcf"
+		}
+		"9"
+		{
+			"type"		"partctrl"
+			"path"		"garrysmod"
+			"name"		"utaunt_sharkfin2_fin2_upright_setter"
+			"pcf"		"particles/summer2025_unusuals.pcf"
+		}
+		"10"
+		{
+			"type"		"header"
+			"text"		"bad: these sprites rotate with the cpoints in"
+		}
+		"11"
+		{
+			"type"		"header"
+			"text"		"portal2 but it doesn't work in gmod; i *think*"
+		}
+		"12"
+		{
+			"type"		"header"
+			"text"		"this is caused by the ''normal'' value just not"
+		}
+		"13"
+		{
+			"type"		"header"
+			"text"		"working on sprites at all in gmod, the sprites"
+		}
+		"14"
+		{
+			"type"		"header"
+			"text"		"are still orientable through other methods"
+		}
+		"15"
+		{
+			"type"		"partctrl"
+			"name"		"tractor_beam_core"
+			"pcf"		"particles/tractor_beam_fx.pcf"
+		}
+		"16"
+		{
+			"type"		"partctrl"
+			"name"		"tractor_beam_endpoint"
+			"pcf"		"particles/tractor_beam_fx.pcf"
+		}
+		"17"
+		{
+			"type"		"partctrl"
+			"name"		"tractor_beam_helix_d"
+			"pcf"		"particles/tractor_beam_fx.pcf"
+		}
+		"18"
+		{
+			"type"		"partctrl"
+			"name"		"tractor_beam_startpoint"
+			"pcf"		"particles/tractor_beam_fx.pcf"
+		}
+		"19"
+		{
+			"type"		"header"
+			"text"		"bad: these sprites render normally in tf2, but"
+		}
+		"20"
+		{
+			"type"		"header"
+			"text"		"in gmod this vmt isn't compatible with "
+		}
+		"21"
+		{
+			"type"		"header"
+			"text"		"orientation_type 3 (have to either use  "
+		}
+		"22"
+		{
+			"type"		"header"
+			"text"		"another vmt, edit the current one, or change"
+		}
+		"23"
+		{
+			"type"		"header"
+			"text"		"the orientation_type; 2 works for all these)"
+		}
+		"24"
+		{
+			"type"		"partctrl"
+			"path"		"tf"
+			"name"		"utaunt_aestheticlogo_blue_logo"
+			"pcf"		"particles/summer2024_unusuals.pcf"
+		}
+		"25"
+		{
+			"type"		"partctrl"
+			"path"		"tf"
+			"name"		"utaunt_aestheticlogo_orange_logo"
+			"pcf"		"particles/summer2024_unusuals.pcf"
+		}
+		"26"
+		{
+			"type"		"partctrl"
+			"path"		"tf"
+			"name"		"utaunt_aestheticlogo_red_logo"
+			"pcf"		"particles/summer2024_unusuals.pcf"
+		}
+		"27"
+		{
+			"type"		"partctrl"
+			"path"		"garrysmod"
+			"name"		"utaunt_tarpit_dinos_8"
+			"pcf"		"particles/halloween2025_unusuals.pcf"
+		}
+		"28"
+		{
+			"type"		"partctrl"
+			"path"		"garrysmod"
+			"name"		"utaunt_tarpit_people_8"
+			"pcf"		"particles/halloween2025_unusuals.pcf"
+		}
+		"29"
+		{
+			"type"		"header"
+			"text"		"bad?: scalar max value is clamped at "
+		}
+		"30"
+		{
+			"type"		"header"
+			"text"		"4096. this still seems almost too large for "
+		}
+		"31"
+		{
+			"type"		"header"
+			"text"		"these fx, but i could still see devs wanting"
+		}
+		"32"
+		{
+			"type"		"header"
+			"text"		"to make even bigger fx, probably not with"
+		}
+		"33"
+		{
+			"type"		"header"
+			"text"		"scalar controls though, so err on the side"
+		}
+		"34"
+		{
+			"type"		"header"
+			"text"		"of player usability for fx that already exist"
+		}
+		"35"
+		{
+			"type"		"partctrl"
+			"name"		"fizzler_field_scalable"
+			"pcf"		"particles/fizzler.pcf"
+		}
+		"36"
+		{
+			"type"		"partctrl"
+			"name"		"spawn_sphere"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+		"37"
+		{
+			"type"		"partctrl"
+			"name"		"spawn_sphere_b"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+		"38"
+		{
+			"type"		"partctrl"
+			"name"		"spawn_sphere_c"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+		"39"
+		{
+			"type"		"partctrl"
+			"name"		"disease_burst"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"40"
+		{
+			"type"		"partctrl"
+			"name"		"disease_burst_b"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"41"
+		{
+			"type"		"partctrl"
+			"name"		"medgrenade_pulse"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"42"
+		{
+			"type"		"partctrl"
+			"name"		"medgrenade_pulse_OLD"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"43"
+		{
+			"type"		"partctrl"
+			"name"		"medgrenade_pulse_dome"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"44"
+		{
+			"type"		"partctrl"
+			"name"		"medgrenade_pulse_glow"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"45"
+		{
+			"type"		"partctrl"
+			"name"		"medgrenade_radius_ring"
+			"pcf"		"particles/buff_medic_fx.pcf"
+		}
+		"46"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_ground_display"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"47"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_pulse"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"48"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_pulse_beams"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"49"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_pulse_echos"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"50"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_pulse_glow"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"51"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_pulse_glow2"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"52"
+		{
+			"type"		"partctrl"
+			"name"		"buffgrenade_radius_ring"
+			"pcf"		"particles/buffgrenade_fx.pcf"
+		}
+		"53"
+		{
+			"type"		"partctrl"
+			"name"		"freeze_grenade_explosion"
+			"pcf"		"particles/freeze_fx.pcf"
+		}
+		"54"
+		{
+			"type"		"partctrl"
+			"name"		"freeze_grenade_explosion_flash"
+			"pcf"		"particles/freeze_fx.pcf"
+		}
+		"55"
+		{
+			"type"		"partctrl"
+			"name"		"freeze_grenade_explosion_smoke"
+			"pcf"		"particles/freeze_fx.pcf"
+		}
+		"56"
+		{
+			"type"		"partctrl"
+			"name"		"freeze_grenade_explosion_warp"
+			"pcf"		"particles/freeze_fx.pcf"
+		}
+		"57"
+		{
+			"type"		"header"
+			"text"		"unintuitive: this really cool oil drip effect is"
+		}
+		"58"
+		{
+			"type"		"header"
+			"text"		"pointing the wrong way by default"
+		}
+		"59"
+		{
+			"type"		"partctrl"
+			"name"		"blood_drip_synth_01b"
+			"pcf"		"particles/blood_impact.pcf"
+		}
+		"60"
+		{
+			"type"		"header"
+			"text"		"unintuitive?: one cpoint only used briefly"
+		}
+		"61"
+		{
+			"type"		"partctrl"
+			"name"		"Glados_Beam"
+			"pcf"		"particles/glados.pcf"
+		}
+		"62"
+		{
+			"type"		"partctrl"
+			"name"		"Advisor_Psychic_Attach_01_finale"
+			"pcf"		"particles/advisor_fx.pcf"
+		}
+		"63"
+		{
+			"type"		"partctrl"
+			"name"		"Advisor_Psychic_Scan_01_finale"
+			"pcf"		"particles/advisor_fx.pcf"
+		}
+		"64"
+		{
+			"type"		"header"
+			"text"		"...cpoint 0 overrides the position of 2 & 3's"
+		}
+		"65"
+		{
+			"type"		"header"
+			"text"		"beam for a split second when the effect "
+		}
+		"66"
+		{
+			"type"		"header"
+			"text"		"starts, then does nothing afterward"
+		}
+		"67"
+		{
+			"type"		"partctrl"
+			"name"		"projected_wall_impact"
+			"pcf"		"particles/projected_wall.pcf"
+		}
+		"68"
+		{
+			"type"		"header"
+			"text"		"...draws a beam between cpoints 1"
+		}
+		"69"
+		{
+			"type"		"header"
+			"text"		"and 2, and then smoke is pulled to 0"
+		}
+		"70"
+		{
+			"type"		"partctrl"
+			"name"		"alien_core_explode_elecropes_blue"
+			"pcf"		"particles/alien_aly_blue_fx.pcf"
+		}
+		"71"
+		{
+			"type"		"partctrl"
+			"name"		"alien_core_explode_elecropes_red"
+			"pcf"		"particles/alien_aly_red_fx.pcf"
+		}
+		"72"
+		{
+			"type"		"header"
+			"text"		"...draws a line between 0 and"
+		}
+		"73"
+		{
+			"type"		"header"
+			"text"		"1, then it gets pulled to 2"
+		}
+		"74"
+		{
+			"type"		"partctrl"
+			"name"		"cleanser_scanline"
+			"pcf"		"particles/projected_wall.pcf"
+		}
+		"75"
+		{
+			"type"		"header"
+			"text"		"unintuitive?: cpoint(s) use a standard"
+		}
+		"76"
+		{
+			"type"		"header"
+			"text"		"constraint but it's not immediately"
+		}
+		"77"
+		{
+			"type"		"header"
+			"text"		"obvious what they're for"
+		}
+		"78"
+		{
+			"type"		"partctrl"
+			"name"		"flamethrower_rainbow_swirl"
+			"pcf"		"particles/flamethrower.pcf"
+		}
+		"79"
+		{
+			"type"		"partctrl"
+			"name"		"portal_rift_03b"
+			"pcf"		"particles/skybox_smoke.pcf"
+		}
+		"80"
+		{
+			"type"		"partctrl"
+			"name"		"portal_rift_03c"
+			"pcf"		"particles/skybox_smoke.pcf"
+		}
+		"81"
+		{
+			"type"		"partctrl"
+			"name"		"portal_rift_03d"
+			"pcf"		"particles/skybox_smoke.pcf"
+		}
+		"82"
+		{
+			"type"		"partctrl"
+			"name"		"portal_rift_03e"
+			"pcf"		"particles/skybox_smoke.pcf"
+		}
+		"83"
+		{
+			"type"		"partctrl"
+			"name"		"portal_rift_03f"
+			"pcf"		"particles/skybox_smoke.pcf"
+		}
+		"84"
+		{
+			"type"		"partctrl"
+			"name"		"test_constraintoverride_2"
+			"pcf"		"particles/partctrl_test.pcf"
+		}
+		"85"
+		{
+			"type"		"partctrl"
+			"name"		"Flies_child2"
+			"pcf"		"particles/insect_fx.pcf"
+		}
+		"86"
+		{
+			"type"		"partctrl"
+			"name"		"utaunt_oceanreef_fishPoint3"
+			"pcf"		"particles/summer2025_unusuals.pcf"
+		}
+		"87"
+		{
+			"type"		"header"
+			"text"		"bad?: some angle controls do nothing;"
+		}
+		"88"
+		{
+			"type"		"header"
+			"text"		"this is a test effect so who cares"
+		}
+		"89"
+		{
+			"type"		"partctrl"
+			"name"		"test_CPointToVector_Ang"
+			"pcf"		"particles/partctrl_test.pcf"
+		}
+		"90"
+		{
+			"type"		"header"
+			"text"		"unintuitive: these stupid things that"
+		}
+		"91"
+		{
+			"type"		"header"
+			"text"		"aren't intended to be used this way"
+		}
+		"92"
+		{
+			"type"		"partctrl"
+			"name"		"test_SetCPoinOffsetToPercentage"
+			"pcf"		"particles/partctrl_test.pcf"
+		}
+		"93"
+		{
+			"type"		"partctrl"
+			"name"		"test_outputAxis_2"
+			"pcf"		"particles/partctrl_test.pcf"
+		}
+		"94"
+		{
+			"type"		"header"
+			"text"		"unintuitive?: i guess we could handle these"
+		}
+		"95"
+		{
+			"type"		"header"
+			"text"		"like distance scalars, but so far i've only"
+		}
+		"96"
+		{
+			"type"		"header"
+			"text"		"found test fx this would apply to, so w/e"
+		}
+		"97"
+		{
+			"type"		"partctrl"
+			"name"		"test_RemapPercBetweenTwoCPointsToVec"
+			"pcf"		"particles/partctrl_test.pcf"
+		}
+		"98"
+		{
+			"type"		"partctrl"
+			"name"		"test_output2_on_parent_goodtest_child"
+			"pcf"		"particles/partctrl_test.pcf"
+		}
+		"99"
+		{
+			"type"		"header"
+			"text"		"???: eight million candle fx"
+		}
+		"100"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_1"
+			"pcf"		"particles/candle.pcf"
+		}
+		"101"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_10"
+			"pcf"		"particles/candle.pcf"
+		}
+		"102"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_11"
+			"pcf"		"particles/candle.pcf"
+		}
+		"103"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_12"
+			"pcf"		"particles/candle.pcf"
+		}
+		"104"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_13"
+			"pcf"		"particles/candle.pcf"
+		}
+		"105"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_14"
+			"pcf"		"particles/candle.pcf"
+		}
+		"106"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_15"
+			"pcf"		"particles/candle.pcf"
+		}
+		"107"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_16"
+			"pcf"		"particles/candle.pcf"
+		}
+		"108"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_17"
+			"pcf"		"particles/candle.pcf"
+		}
+		"109"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_18"
+			"pcf"		"particles/candle.pcf"
+		}
+		"110"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_19"
+			"pcf"		"particles/candle.pcf"
+		}
+		"111"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_2"
+			"pcf"		"particles/candle.pcf"
+		}
+		"112"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_20"
+			"pcf"		"particles/candle.pcf"
+		}
+		"113"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_21"
+			"pcf"		"particles/candle.pcf"
+		}
+		"114"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_22"
+			"pcf"		"particles/candle.pcf"
+		}
+		"115"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_23"
+			"pcf"		"particles/candle.pcf"
+		}
+		"116"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_24"
+			"pcf"		"particles/candle.pcf"
+		}
+		"117"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_25"
+			"pcf"		"particles/candle.pcf"
+		}
+		"118"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_26"
+			"pcf"		"particles/candle.pcf"
+		}
+		"119"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_27"
+			"pcf"		"particles/candle.pcf"
+		}
+		"120"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_28"
+			"pcf"		"particles/candle.pcf"
+		}
+		"121"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_29"
+			"pcf"		"particles/candle.pcf"
+		}
+		"122"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_3"
+			"pcf"		"particles/candle.pcf"
+		}
+		"123"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_30"
+			"pcf"		"particles/candle.pcf"
+		}
+		"124"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_31"
+			"pcf"		"particles/candle.pcf"
+		}
+		"125"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_32"
+			"pcf"		"particles/candle.pcf"
+		}
+		"126"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_33"
+			"pcf"		"particles/candle.pcf"
+		}
+		"127"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_34"
+			"pcf"		"particles/candle.pcf"
+		}
+		"128"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_35"
+			"pcf"		"particles/candle.pcf"
+		}
+		"129"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_36"
+			"pcf"		"particles/candle.pcf"
+		}
+		"130"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_37"
+			"pcf"		"particles/candle.pcf"
+		}
+		"131"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_38"
+			"pcf"		"particles/candle.pcf"
+		}
+		"132"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_39"
+			"pcf"		"particles/candle.pcf"
+		}
+		"133"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_4"
+			"pcf"		"particles/candle.pcf"
+		}
+		"134"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_40"
+			"pcf"		"particles/candle.pcf"
+		}
+		"135"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_41"
+			"pcf"		"particles/candle.pcf"
+		}
+		"136"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_42"
+			"pcf"		"particles/candle.pcf"
+		}
+		"137"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_43"
+			"pcf"		"particles/candle.pcf"
+		}
+		"138"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_44"
+			"pcf"		"particles/candle.pcf"
+		}
+		"139"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_45"
+			"pcf"		"particles/candle.pcf"
+		}
+		"140"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_46"
+			"pcf"		"particles/candle.pcf"
+		}
+		"141"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_47"
+			"pcf"		"particles/candle.pcf"
+		}
+		"142"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_48"
+			"pcf"		"particles/candle.pcf"
+		}
+		"143"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_49"
+			"pcf"		"particles/candle.pcf"
+		}
+		"144"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_5"
+			"pcf"		"particles/candle.pcf"
+		}
+		"145"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_50"
+			"pcf"		"particles/candle.pcf"
+		}
+		"146"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_6"
+			"pcf"		"particles/candle.pcf"
+		}
+		"147"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_7"
+			"pcf"		"particles/candle.pcf"
+		}
+		"148"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_8"
+			"pcf"		"particles/candle.pcf"
+		}
+		"149"
+		{
+			"type"		"partctrl"
+			"name"		"Candle_spot_9"
+			"pcf"		"particles/candle.pcf"
+		}
+		"150"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_flame1"
+			"pcf"		"particles/candle.pcf"
+		}
+		"151"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master"
+			"pcf"		"particles/candle.pcf"
+		}
+		"152"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master_1"
+			"pcf"		"particles/candle.pcf"
+		}
+		"153"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master_10"
+			"pcf"		"particles/candle.pcf"
+		}
+		"154"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master_25"
+			"pcf"		"particles/candle.pcf"
+		}
+		"155"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master_5"
+			"pcf"		"particles/candle.pcf"
+		}
+		"156"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master_50"
+			"pcf"		"particles/candle.pcf"
+		}
+		"157"
+		{
+			"type"		"partctrl"
+			"name"		"candleset_master_flare"
+			"pcf"		"particles/candle.pcf"
+		}
+		"158"
+		{
+			"type"		"header"
+			"text"		"???: weird l4d2 fx that are either unused"
+		}
+		"159"
+		{
+			"type"		"header"
+			"text"		"or not intended to be used this way"
+		}
+		"160"
+		{
+			"type"		"header"
+			"text"		"bad?: scalar stops doing anything past 1.0?"
+		}
+		"161"
+		{
+			"type"		"partctrl"
+			"name"		"glow_boomer_it_victim"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+		"162"
+		{
+			"type"		"header"
+			"text"		"bad?: cpoint 0 doesn't appear to do anything"
+		}
+		"163"
+		{
+			"type"		"partctrl"
+			"name"		"glow_survivor_hanging_from_ledge"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+		"164"
+		{
+			"type"		"partctrl"
+			"name"		"glow_survivor_hanging_from_tongue"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+		"165"
+		{
+			"type"		"header"
+			"text"		"bad?: neither cpoint 0 or color control"
+		}
+		"166"
+		{
+			"type"		"header"
+			"text"		"appear to do anything; color control seems"
+		}
+		"167"
+		{
+			"type"		"header"
+			"text"		"funky because 2 axes have the same outMin"
+		}
+		"168"
+		{
+			"type"		"header"
+			"text"		"and outMax"
+		}
+		"169"
+		{
+			"type"		"partctrl"
+			"name"		"glow_survivor_use_target"
+			"pcf"		"particles/ui_fx.pcf"
+		}
+	}
+	"name"		"Low priority / not actionable"
+	"version"		"3"
+}
